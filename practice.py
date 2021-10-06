ee3abe6f39c4629a90c8ce179b849d360d25db5b
06hong <86993454+06hong@users.noreply.github.com>
2021-10-05 21:55:30 -0400

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,17 @@
+Python 3.9.5 (v3.9.5:0a7dcbdb13, May  3 2021, 13:17:02) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> hello_world.py
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    hello_world.py
+NameError: name 'hello_world' is not defined
+>>> print("Hello Python world!")
+Hello Python world!
+>>> message=("efef")
+>>> print(message)
+efef
+>>> message=("yolo")
+>>> print(message)
+yolo
+>>> 
